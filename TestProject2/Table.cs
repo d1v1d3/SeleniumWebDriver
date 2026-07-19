c7a8176c13ee2f9f5edeaa09204b0ed4f32918ae
unknown <marinovjulien@gmail.com>
2026-07-19 16:44:24 +0300

    Added continuous integration

--- TestProject2/Table.cs
+++ TestProject2/Table.cs
@@ -14,7 +14,14 @@ namespace TestProject2
         public void SetUp()
         {
             // Create object of ChromeDriver
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("headless");
+            options.AddArguments("no-sandbox");
+            options.AddArguments("disable-dev-shm-usage");
+            options.AddArguments("disable-gpu");
+            options.AddArguments("window-size=1920x1080");
+            options.AddArguments("remote-debugging-port=9222");
+            driver = new ChromeDriver(options);
 
             // Add implicit wait
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
